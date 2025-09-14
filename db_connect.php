73f41ecbbfd9cbdd5d83f5a7c8d22aa5ea795406
shilconsultancy <shilconsultancy@gmail.com>
2025-09-14 14:56:06 +0600

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,8 +2,8 @@
 // db_connect.php
 
 $servername = "localhost"; // Your database server name, usually 'localhost'
-$username = "u324770578_admin";     // Your database username
-$password = "c*ST;+A3";         // Your database password
+$username = "root";     // Your database username
+$password = "";         // Your database password
 $dbname = "u324770578_booking"; // The name of the database you created
 
 // Create connection
